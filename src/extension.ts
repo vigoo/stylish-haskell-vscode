19ad3dc9531b5c8c4ee6511d34fd4f1fd7b8b683
Daniel Vigovszky <daniel.vigovszky@gmail.com>
2015-12-18 19:11:06 +0100

    Configurable executable path, showing errors

--- src/extension.ts
+++ src/extension.ts
@@ -7,14 +7,26 @@ export function activate(context: vscode.ExtensionContext) {
 
 	var disposable = vscode.commands.registerCommand('stylishHaskell.runOnCurrent', () => {
 		proc.exec(
-			"stylish-haskell -i " + vscode.window.activeTextEditor.document.fileName,
+			stylishHaskellCmd() + " -i " + vscode.window.activeTextEditor.document.fileName,
 			(error: Error, stdout: Buffer, stderr: Buffer) => {
 				if (error) {
 					vscode.window.showErrorMessage("Failed to run stylish-haskell");
+				}
+				
+				if (stderr.length > 0) {
+					var channel = vscode.window.createOutputChannel('stylish-haskell');
+					channel.clear();
+					channel.appendLine(stderr.toString());
+					channel.show(vscode.ViewColumn.Two);
 				}
 			}
 		);
 	});
 	
 	context.subscriptions.push(disposable);
+}
+
+function stylishHaskellCmd() {
+    var config = vscode.workspace.getConfiguration("stylishHaskell");
+    return config["commandLine"];
 }
